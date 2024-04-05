5373e7aca0d462bf241211bca1e16e6e128bd073
sara-rioseco <sara.rioseco@gmail.com>
2024-04-05 12:44:57 -0300

    add text to presentation section in home page

--- src/components/home/Presentation/Presentation.tsx
+++ src/components/home/Presentation/Presentation.tsx
@@ -6,12 +6,20 @@ export const Presentation = () => {
   return (
     <section className={styles.Presentation}>
       <div>
-        <h2>Here&apos;s my pitch and picture :D</h2>
+        <h2>Hello, My name is Sara.</h2>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, fugit.
-          Illo, et sed eum ducimus quod dolores, doloribus quisquam dignissimos
-          deserunt nostrum, repellendus cupiditate ad minus quia veniam rerum
-          sapiente.
+          I used to be a translator, but those days are over and gone. Now
+          I&apos;m doing something I&apos;m more passionate about, and yes, you
+          guessed it right: it&apos;s <strong>Web Developing</strong>!
+        </p>
+        <p>
+          It was in late 2022 when I started this path and, since then,
+          I&apos;ve been studying and doing some very fun projects. Please check
+          the <strong>Projects</strong> section to see more about that.
+        </p>
+        <p>
+          You can find more information about me and my experience on the{' '}
+          <strong>Resume</strong> / <strong>About Me</strong> sections.
         </p>
       </div>
       <div className={styles.Presentation__imageContainer}>
